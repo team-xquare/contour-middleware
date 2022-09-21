14856b60216ef70b9a640cbf7fa986488d958b17
HoWon <56785132+hwc9169@users.noreply.github.com>
2022-09-21 17:51:33 +0900

    🧪 :: jwt test (#11)

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -16,9 +16,9 @@ type JWTClaims struct {
 	jwt.StandardClaims
 }
 
-func (c JWTClaims) ToJWTToken() string {
+func (c *JWTClaims) ToJWTToken() string {
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
-	token, _ := at.SignedString(jwtSecret)
+	token, _ := at.SignedString([]byte(jwtSecret))
 
 	return token
 }
